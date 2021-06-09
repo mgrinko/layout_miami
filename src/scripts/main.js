f4819ef54e309ab95d2713228cc6f54dba33e75b
mgrinko <glukym@gmail.com>
2021-06-09 06:30:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const menu = document.querySelector('.page__menu');
+const menuOpener = document.querySelector('.menu-toggler--opener');
+const menuCloser = document.querySelector('.menu-toggler--closer');
+
+menuOpener.addEventListener('click', () => {
+  menu.classList.add('page__menu--open');
+});
+
+menuCloser.addEventListener('click', () => {
+  menu.classList.remove('page__menu--open');
+});
