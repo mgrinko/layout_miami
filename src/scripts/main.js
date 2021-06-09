da272f0eb7c4687c44487409374cb3c82860d6ca
mgrinko <glukym@gmail.com>
2021-06-09 08:21:26 +0300

    mobile version was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const menu = document.querySelector('.page__menu');
+const menuList = document.querySelector('.menu__list');
+
 const menuOpener = document.querySelector('.menu-toggler--opener');
 const menuCloser = document.querySelector('.menu-toggler--closer');
 
@@ -10,4 +12,10 @@ menuOpener.addEventListener('click', () => {
 
 menuCloser.addEventListener('click', () => {
   menu.classList.remove('page__menu--open');
+});
+
+menuList.addEventListener('click', (event) => {
+  if (event.target.href) {
+    menu.classList.remove('page__menu--open');
+  }
 });
